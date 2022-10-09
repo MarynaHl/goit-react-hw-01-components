0de3d0971832e097f0327eb94c33d2f1d625b231
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-10-09 12:19:12 +0100

    task-01

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Section.module.css'
+import Container from '../Container/Container';
+
+const Section = ({children, title}) => {
+  return (
+    <div className={s.container}>
+      <Container styles={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+        <h2 style={{
+          alignSelf: 'stretch',
+          marginBottom: 20,
+        }}>{title}</h2>
+        {children}
+      </Container>
+    </div>
+  );
+};
+
+Container.propTypes = {
+  title: PropTypes.string
+};
+
+export default Section;
