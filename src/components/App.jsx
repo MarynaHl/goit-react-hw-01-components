7a78e0c0fe7c9b785011b359f3c8ecf2b5e04548
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-10-09 12:16:01 +0100

    task-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
+import ProfileCard from './ProfileCard/ProfileCard';
+import user from '../json/user.json'
+import statistic from '../json/statistic.json'
+import friends from '../json/friends.json'
+import transactions from '../json/transactions.json'
+import Section from './Section/Section';
+import Statistic from './Statistic/Statistic';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section
+        title='Task 1. Profile'
+      >
+        <ProfileCard
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+
+      <Section
+        title='Task 2. Statistics'
+      >
+        <Statistic
+          statisticList={statistic}
+        />
+      </Section>
+
+      <Section
+        title='Task 3. Friend list'
+      >
+        <FriendList
+          friends={friends}
+        />
+      </Section>
+
+      <Section
+        title='Task 4. Transaction history'
+      >
+        <TransactionHistory
+          items={transactions}
+        />
+      </Section>
+    </>
   );
 };
